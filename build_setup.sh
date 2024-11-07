1ee92186529a5e92a695c0f6fe6053a788890408
chenyisong <chenyisong@tuya.com>
2024-11-07 20:12:16 +0800

    update script

--- build_setup.sh
+++ build_setup.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-TARGET=$1
+
+PROJ_NAME=$1
+TARGET=$2
+CHIP=$3
 echo TARGET=$TARGET
 if [ x"$TARGET" = x"" ]; then
-    echo "Usage: <./ platform_prepare.sh esp32|esp32s2|esp32s3|esp32c2|esp32c3|esp32c6>"
     exit 1
 fi
 
-TOP_DIR=$(pwd)
+ 
+echo PROJ_NAME=$PROJ_NAME
+echo TARGET=$TARGET
+echo CHIP=$CHIP
+
+TOP_DIR=$(dirname "$0")
 echo $TOP_DIR
 
-echo "Start platform prepare ..."
-
-rm -rf ${TOP_DIR}/.target
-
-rm -rf ${TOP_DIR}/tuya_open_sdk/sdkconfig
-rm -rf ${TOP_DIR}/tuya_open_sdk/sdkconfig.old
-rm -rf ${TOP_DIR}/tuya_open_sdk/sdkconfig.defaults
-rm -rf ${TOP_DIR}/tuya_open_sdk/build
+echo "Start board build_setup ..."
 
 MORROR=0
 
@@ -49,6 +49,7 @@ function get_country_code()
 function enable_mirror()
 {
     get_country_code
+
     if [ x"$MORROR" = x"1" ]; then
         echo "enable mirror"
         if [ ! -d ${IDF_GITEE_TOOLS_PATH} ]; then
@@ -80,6 +81,23 @@ function download_esp_idf()
     return 0
 }
 
+function download_esp_idf_tools()
+{
+    echo "download esp-idf-tools ..."
+
+    cd ${IDF_PATH}
+    git submodule update --init --recursive
+    cd -
+        
+    if [ x"$MORROR" = x"1" ]; then
+        bash ${IDF_GITEE_TOOLS_PATH}/install.sh ${IDF_PATH}
+    else
+        bash ${IDF_PATH}/install.sh all
+    fi
+
+}
+
+
 if [ ! -d ${IDF_PATH} ]; then
     echo "IDF_PATH is empty"
     
@@ -89,9 +107,34 @@ if [ ! -d ${IDF_PATH} ]; then
     if [ $? -ne 0 ]; then
         echo "git clone esp-idf failed ..."
         exit 1
+    else
+        download_esp_idf_tools
     fi
 fi
 
-disable_mirror
+if [ ! -d ${IDF_TOOLS_PATH} ] || [ ! -d ${IDF_TOOLS_PATH}/tools ];then
+    echo "IDF_TOOLS_PATH is empty ..."
 
-echo "Run platform prepare success ..."
+    enable_mirror
+    download_esp_idf_tools
+fi
+
+if [ x"$CHIP" = x"esp32" ]; then
+    TOOLCHAIN_PATH=${IDF_TOOLS_PATH}/tools/xtensa-esp-elf
+elif [ x"$CHIP" = x"esp32c3" ]; then
+    TOOLCHAIN_PATH=${IDF_TOOLS_PATH}/tools/riscv32-esp-elf
+else
+    echo "Target $TARGET not support"
+    exit 1
+fi
+
+if [ ! -d ${TOOLCHAIN_PATH} ]; then
+    echo "TOOLCHAIN_PATH is empty ..."
+    enable_mirror
+    download_esp_idf_tools
+fi
+
+disable_mirror
+echo "Run build setup success ..."
+
+exit 0
