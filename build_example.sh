3fd4dd903c03a9451611b588ec96efe100bf830d
KyleDeng <dlkxiaok@163.com>
2025-04-02 16:23:22 +0800

    update examples

--- build_example.sh
+++ build_example.sh
@@ -78,6 +78,9 @@ if command -v idf.py &>/dev/null; then
     echo "Use existing esp-idf tools."
 else
     IDF_PATH=${TOP_DIR}/esp-idf
+    IDF_TOOLS_PATH=${TOP_DIR}/.espressif
+    export IDF_PATH
+    export IDF_TOOLS_PATH
     cd ${IDF_PATH}
     . export.sh > /dev/null
     cd -
