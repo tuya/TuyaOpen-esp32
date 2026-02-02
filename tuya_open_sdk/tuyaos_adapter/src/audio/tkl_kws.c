3062b35895f35ff8b87b5ed9189c72922795b75b
YangJie <shiliu.yang@tuya.com>
2026-02-02 18:51:47 +0800

    Delete tkl_asr.h, and add tkl_kws.h and tkl_kws.c.

--- tuya_open_sdk/tuyaos_adapter/src/audio/tkl_kws.c
+++ tuya_open_sdk/tuyaos_adapter/src/audio/tkl_kws.c
@@ -1,6 +1,6 @@
 /**
- * @file tkl_asr.c
- * @brief tkl_asr module is used to 
+ * @file tkl_kws.c
+ * @brief tkl_kws module is used to 
  * @version 0.1
  * @date 2025-04-15
  */
@@ -11,12 +11,12 @@
 #include "esp_err.h"
 #include "esp_log.h"
 
-#include "tkl_asr.h"
+#include "tkl_kws.h"
 
 /***********************************************************
 ************************macro define************************
 ***********************************************************/
-#define TAG "tkl_asr"
+#define TAG "tkl_kws"
 
 /***********************************************************
 ***********************typedef define***********************
@@ -31,48 +31,48 @@
 /***********************************************************
 ***********************variable define**********************
 ***********************************************************/
-static TKL_ASR_WAKEUP_CB sg_wakeup_cb = NULL;
-static bool sg_is_asr_enable = false;
+static TKL_KWS_WAKEUP_CB sg_wakeup_cb = NULL;
+static bool sg_is_kws_enable = false;
 
 /***********************************************************
 ***********************function define**********************
 ***********************************************************/
 static void __afe_wakeup_callback(void)
 {
-    if(true == sg_is_asr_enable && sg_wakeup_cb) {
-        sg_wakeup_cb(TKL_ASR_WAKEUP_NIHAO_XIAOZHI);
+    if(true == sg_is_kws_enable && sg_wakeup_cb) {
+        sg_wakeup_cb(TKL_KWS_WAKEUP_NIHAO_XIAOZHI);
     }
 }
 
-OPERATE_RET tkl_asr_init(void)
+OPERATE_RET tkl_kws_init(void)
 {
     auio_afe_register_wakeup_cb(__afe_wakeup_callback);
 
     return OPRT_OK;
 }
 
-OPERATE_RET tkl_asr_reg_wakeup_cb(TKL_ASR_WAKEUP_CB wakeup_cb)
+OPERATE_RET tkl_kws_reg_wakeup_cb(TKL_KWS_WAKEUP_CB wakeup_cb)
 {
     sg_wakeup_cb = wakeup_cb;
 
     return OPRT_OK;
 }
 
-OPERATE_RET tkl_asr_enable(void)
+OPERATE_RET tkl_kws_enable(void)
 {
-    sg_is_asr_enable = true;
+    sg_is_kws_enable = true;
 
     return OPRT_OK;
 }
 
-OPERATE_RET tkl_asr_disable(void)
+OPERATE_RET tkl_kws_disable(void)
 {
-    sg_is_asr_enable = false;
+    sg_is_kws_enable = false;
 
     return OPRT_OK;
 }
 
-OPERATE_RET tkl_asr_deinit(void)
+OPERATE_RET tkl_kws_deinit(void)
 {
     return OPRT_OK;
 }
